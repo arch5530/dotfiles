851d50de89df2cc46d6c80ceb636ea04d2f2c253
arch5530 <65083721+arch5530@users.noreply.github.com>
2020-06-06 12:39:38 -0400

    Add files via upload

---
+++ .mail.sh
@@ -0,0 +1,14 @@
+USER=a.carpin86@gmail.com
+PASS=LesPaul86!
+  
+COUNT=`curl -su $USER:$PASS https://mail.google.com/mail/feed/atom || echo "<fullcount>unknown number of</fullcount>"`
+COUNT=`echo "$COUNT" | grep -oPm1 "(?<=<fullcount>)[^<]+" `
+echo $COUNT
+if [ "$COUNT" != "0" ]; then
+   if [ "$COUNT" = "1" ];then
+      WORD="mail";
+   else
+      WORD="mails";
+   fi
+fi
+
